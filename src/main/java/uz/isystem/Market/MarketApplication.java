040afd355d5833272da39fb55fad1994d2fcbd3e
Sadullayev06 <sadullayevzufarbek4@gmail.com>
2022-05-26 15:44:34 +0500

    update

--- src/main/java/uz/isystem/Market/MarketApplication.java
+++ src/main/java/uz/isystem/Market/MarketApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketApplication {
+	//salom
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
