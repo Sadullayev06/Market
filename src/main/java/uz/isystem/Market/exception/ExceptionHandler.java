88d38538ea6fad8dcdab7db09d666d5640ceaad9
burxon <asrorovqodirjon2005@gmail.com>
2022-05-27 18:35:16 +0500

    chutki

---
+++ src/main/java/uz/isystem/Market/exception/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package uz.isystem.Market.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+
+}
