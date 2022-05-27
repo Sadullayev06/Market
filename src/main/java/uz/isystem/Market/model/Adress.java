96535e4e4bcdc935a77555a1d41dca62425cdc5a
burxon <asrorovqodirjon2005@gmail.com>
2022-05-27 18:28:33 +0500

    chutki

--- src/main/java/uz/isystem/Market/model/Adress.java
+++ src/main/java/uz/isystem/Market/model/Adress.java
@@ -1,4 +1,15 @@
 package uz.isystem.Market.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = ("adresses"))
 public class Adress {
+
 }
